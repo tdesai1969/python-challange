098bd66874d3784dbfe0a7e168eb6d5126cfa77f
Tej <tej_@hotmail.com>
2019-09-10 17:11:18 -0500

    Better Version of PyBank + First commit for PyPoll

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,52 +2,48 @@
 import os
 import csv
 
+#Variable
 month = []
-profit = []
-loss = []
-totalList=[]
-
-sum=0
-sum1=0
-sum2=0
+totalprofit=[]
+mon_pro_cha=[]
 
 # Set path for file
 csvpath = os.path.join("..", "PyBank","Resources","budget_data.csv")
 
-# Open the CSV
+# Open the CSV File
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile , delimiter=",")
     csv_header = next(csvfile)
-    print(f"header {csv_header }")
+    #print(f"header {csv_header }")
+
     for row in csvreader:
-    
         month.append(row[0])
-
-        totalList.append(float(row[1]))
-
-        #print ("Month ::"+ row[0])
-        #print("PROFIT/LOSS::"+row[1])
-      
-        if  float(row[1]) > 0: 
-           profit.append(float (row[1]))
-          # print("profit:"+row[1])
-        else: 
-            loss.append(float (row[1]))
+        totalprofit.append(float(row[1]))
         
-    for num in totalList:
-        sum = sum +num
-    average  = sum / len(totalList)
+# Iterate through the profits in order to get the monthly change in profits
 
-    for pronum in profit:
-        sum1 = sum1+pronum
-    averageprofit = sum1 / len(profit)
+    for x in range(len(totalprofit)-1):
+        mon_pro_cha.append(totalprofit[x+1]-totalprofit[x])
 
-    for losnum in loss:
-        sum2 = sum2+losnum
-    averageloss = sum2 / len(loss)
+# Obtain the max and min of the the montly profit change list
+        max_increase_profit = max(mon_pro_cha)
+        max_decrease_profit = min(mon_pro_cha)
 
-    print ("Average of profit: ", round(averageprofit,2) )
-    print ("Average of loss: ", round(averageloss,2) )
-    print ("Total $ : ", sum)
-    print ("Average of list element is: ", round(average,2) )
+# Print on Terminal
     print ("Total number of Month: ", len(month) )
+    print(f"Total: ${sum(totalprofit)}")
+    print(f"Average Change: {round(sum(mon_pro_cha)/len(mon_pro_cha),2)}")
+    print(f"Greatest Increase in Profits: $" + str(max_increase_profit))
+    print(f"Greatest Decrease in Profits: $" + str(max_decrease_profit))
+
+# Use write function to create and write to text file
+output_path = os.path.join("..", "PyBank","Resources","PyBankText.txt")
+with open(output_path, 'w', newline='') as file:
+    file.write("Financial Analysis \n")
+    file.write("------------------------- \n")
+    file.write("Total Months:" + str(len(month)) + "\n")
+    file.write(f"Total: ${sum(totalprofit)}" + "\n")
+    file.write(f"Average Change: {round(sum(mon_pro_cha)/len(mon_pro_cha),2)}" + "\n")
+    file.write(f"Greatest Increase in Profits: $" + str(max_increase_profit) + "\n")
+    file.write(f"Greatest Decrease in Profits: $" + str(max_decrease_profit) + "\n")
+    file.write("------------------------- ")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,67 @@
-print("main file")
+#Modules
+import os
+import csv
+
+#Variable
+# voterid = []
+# county = []
+# candidate = []
+total_vote=0
+khan_vote=0
+correy_vote=0
+li_vote=0
+otooley_vote=0
+
+# Set path for file
+csvpath = os.path.join("..", "PyPoll","Resources","election_data.csv")
+
+# Open the CSV File
+with open(csvpath, newline="") as csvfile:
+    csvreader = csv.reader(csvfile , delimiter=",")
+    csv_header = next(csvfile)
+    print(f"header {csv_header }")
+
+    for row in csvreader:
+        # voterid.append(row[0])
+        # county.append(row[1])
+        # candidate.append(row[2])
+        total_vote +=1
+        if row[2] == "Khan": 
+            khan_vote += 1
+        elif row[2] == "Correy":
+            correy_vote +=1
+        elif row[2] == "Li": 
+            li_vote +=1
+        elif row[2] == "O'Tooley":
+            otooley_vote +=1
+
+# % Calcualtions
+khan_percent = (khan_vote/total_vote) *100
+correy_percent = (correy_vote/total_vote) * 100
+li_percent = (li_vote/total_vote)* 100
+otooley_percent = (otooley_vote/total_vote) * 100
+
+# # Print Vote + Vote % on Terminal
+print(f"Total Vote: ", total_vote )
+print(f"Khan Votes: ", khan_vote )
+print(f"Correy Votes:", correy_vote )
+print(f"Li Votes:", li_vote )
+print(f"O'Tooley Votes:", otooley_vote )
+
+print(f"Khan % Votes: ", khan_percent )
+print(f"Correy % Votes:", correy_percent )
+print(f"Li % Votes", li_percent )
+print(f"O' % Votes:", otooley_percent )
+
+
+# # Use write function to create and write to text file
+# output_path = os.path.join("..", "PyPoll","Resources","PyPollText.txt")
+# with open(output_path, 'w', newline='') as file:
+#     file.write("Financial Analysis \n")
+#     file.write("------------------------- \n")
+#     file.write("Total Months:" + str(len(month)) + "\n")
+#     file.write(f"Total: ${sum(totalprofit)}" + "\n")
+#     file.write(f"Average Change: {round(sum(mon_pro_cha)/len(mon_pro_cha),2)}" + "\n")
+#     file.write(f"Greatest Increase in Profits: $" + str(max_increase_profit) + "\n")
+#     file.write(f"Greatest Decrease in Profits: $" + str(max_decrease_profit) + "\n")
+#     file.write("------------------------- ")
